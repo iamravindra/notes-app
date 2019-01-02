b6a4437f374f56cf09fac97b074beda4bd46fa64
Ravindra shekhawat <shekhawat.7924@gmail.com>
2019-01-03 00:16:14 +0530

    update commit

--- app.js
+++ app.js
@@ -9,4 +9,20 @@ let title = yargs.argv.title;
 let body = yargs.argv.body;
 let command = yargs.argv._[0];
 
-console.log(title, body, command);
+
+
+if (command === 'add') {
+    console.log('adding note');
+    notes.addingNote(title, body);
+} else if (command == 'remove') {
+    console.log('removing note');
+    notes.removeNote(title);
+} else if (command === 'read') {
+    console.log('reading note');
+    notes.readNote(title);
+} else if (command === 'list') {
+    console.log('listing all notes');
+    notes.getAll();
+} else {
+    console.log('unknown command was used!!');
+}
--- notes.js
+++ notes.js
@@ -1,1 +1,68 @@
-console.log('hi');
+const fs = require('fs');
+
+
+var fetchNotes = () => {
+    try {
+        return JSON.parse(fs.readFileSync('notes.txt'));
+    } catch (err) {
+        return [];
+    }
+}
+
+
+var addingNote = (title, body) => {
+    var notes = fetchNotes();
+
+    var note = {
+        title,
+        body
+    };
+
+    var double = notes.filter((note) => note.title === title);
+    if (double.length == 0) {
+        notes.push(note);
+        fs.writeFileSync('notes.txt', JSON.stringify(notes));
+    } else {
+        console.log(`Stop: Title alredy exists`);
+    }
+
+
+    logNote(note);
+}
+
+var removeNote = (title) => {
+    var notes = fetchNotes();
+
+    var filterNotes = notes.filter((note) =>
+        note.title !== title
+    );
+    fs.writeFileSync('notes.txt', JSON.stringify(filterNotes));
+}
+
+var readNote = (title) => {
+    var notes = fetchNotes();
+    var filterNotes = notes.filter((note) =>
+        note.title === title
+    );
+    logNote(filterNotes[0]);
+
+}
+
+var getAll = () => {
+    var notes = fetchNotes();
+
+    notes.forEach(note => logNote(note));
+}
+
+var logNote = (note) => {
+    console.log("*********************************");
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+}
+
+module.exports = {
+    addingNote,
+    removeNote,
+    readNote,
+    getAll
+}
